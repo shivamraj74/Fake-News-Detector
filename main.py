8ed1acc329a35ee69bb2b656a0ec6429cba5825c
Shivam Raj <48693624+shivamraj74@users.noreply.github.com>
2020-10-11 16:28:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pickle
+var = input("Please enter the news text you want to verify: ")
+print("You entered: " + str(var))
+
+
+# function to run for prediction
+def fakenews(var):    
+# retrieving the best model for prediction call
+    load_model = pickle.load(open('A:\data\model.sav', 'rb'))
+    prediction = load_model.predict([var])
+    prob = load_model.predict_proba([var])
+
+    return (print("The given statement is ",prediction[0]),
+        print("The truth probability score is ",prob[0][1]))
+
+
+if name == 'main':
+    fakenews(var)
